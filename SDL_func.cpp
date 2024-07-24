04e1715cedbfa28459d7b5ec927a02471d52190d
khanhsk67 <125454765+khanhsk67@users.noreply.github.com>
2024-07-24 23:10:56 +0700

    Add files via upload

---
+++ SDL_func.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+#include "SDL_func.hpp"
+using namespace std;
+
+void initSDL(SDL_Window*& window, SDL_Renderer*& renderer)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logSDLError(std::cout, "SDL_Init", true);
+
+    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,
+        SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+        SDL_RENDERER_PRESENTVSYNC);
+
+    if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+
+
+void logSDLError(std::ostream& os,
+    const std::string& msg, bool fatal)
+{
+    os << msg << " Error: " << SDL_GetError() << std::endl;
+    if (fatal) {
+        SDL_Quit();
+        exit(1);
+    }
+}
+
+
