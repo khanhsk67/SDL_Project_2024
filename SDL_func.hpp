54b0a5d7f143a806f769df5305333961a2562a5a
khanhsk67 <125454765+khanhsk67@users.noreply.github.com>
2024-08-14 14:49:45 +0700

    Add files via upload

--- SDL_func.hpp
+++ SDL_func.hpp
@@ -23,12 +23,6 @@ void waitUntilKeyPressed();
 
 bool dieuhuong(SDL_Rect& filled_rect, int&, int&);
 
-SDL_Texture* loadtexture(string path, SDL_Renderer* renderer);
 
-void HuyTexture(SDL_Texture* texture);
-
-SDL_Rect Excalibur();
-
-void AutoMove(SDL_Rect& rect);
 
 #endif // SDl_func
